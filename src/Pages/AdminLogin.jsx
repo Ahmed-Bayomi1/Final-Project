096d41429f5357e224a510bfb9df847ae55a61ae
Ahmed-Bayomi1 <ahmedbayomihashem@gmail.com>
2026-07-04 09:17:18 +0300

    first step

--- src/Pages/AdminLogin.jsx
+++ src/Pages/AdminLogin.jsx
@@ -5,7 +5,6 @@ import Navbar from "../Component/Navbar";
 export default function AdminLogin(){
     return(
         <div>
-            <Navbar/>
             <h3>AdminLogin</h3>
         </div>
     ); 
